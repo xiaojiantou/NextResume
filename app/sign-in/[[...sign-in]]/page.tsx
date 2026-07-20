e3d29438b83e6247bb65d874ff5432541dd352a1
xiaojiantou <xiaojiantou@outlook.com>
2026-07-20 15:52:02 -0700

    Deploy: 2026-07-20 15:52:02

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <SignIn />
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <div className="w-full max-w-md">
+        <SignIn
+          appearance={{
+            elements: {
+              rootBox: "w-full",
+              card: "w-full",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
